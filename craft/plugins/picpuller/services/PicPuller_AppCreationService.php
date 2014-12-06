0770740891af2aef5b490fb0104b4d118d02adde
John Morton <john@johnfmorton.com>
2014-12-06 11:44:33 -0500

    Removed spacing

--- craft/plugins/picpuller/services/PicPuller_AppCreationService.php
+++ craft/plugins/picpuller/services/PicPuller_AppCreationService.php
@@ -78,7 +78,7 @@ class PicPuller_AppCreationService extends BaseApplicationComponent
     /**
      * Save an oAuth record into the database
      * @param  PicPuller_OauthModel $model An instance of an oAuth model
-     * @return BOOL                      True or false depending on success
+     * @return BOOL True or false depending on success
      */
     public function saveOauth(PicPuller_OauthModel &$model) {
         $record = $this->oauthRecord->create();
